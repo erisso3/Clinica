0a185c337ac4340bab1b091eb18dd272301f50be
erick <erisso.333@hotmail.com>
2021-04-20 15:01:57 -0500

    Modales

--- Clinica/Controllers/CitasController.cs
+++ Clinica/Controllers/CitasController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Clinica.Objects;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,9 +20,43 @@ namespace Clinica.Controllers
             return View();
         }
 
-        public ActionResult DetallesCita()
+        /*public ActionResult DetallesCita()
         {
             return View();
+        }*/
+
+        public async Task<ActionResult> DetallesCita()
+        {
+            List<Medicamento> medicamentos = new List<Medicamento>();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    //client.BaseAddress = new Uri("http://localhost:3000");
+                    client.BaseAddress = new Uri("https://apimedicamentos.herokuapp.com/");
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage resp = await client.GetAsync("/");
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        var medResponde = resp.Content.ReadAsStringAsync().Result;
+                        medicamentos = JsonConvert.DeserializeObject<List<Medicamento>>(medResponde);
+                        System.Diagnostics.Debug.WriteLine("Exito");
+
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("No Exito");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                System.Diagnostics.Debug.WriteLine("No Exito");
+            }
+            return View(medicamentos);
         }
     }
 }
